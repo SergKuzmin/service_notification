b8bdef801a3e63fc58517777251b85d73c4c66ca
s.kuzminyh@expasoft.tech <kuzminyh120@mail.ru>
2021-07-12 18:03:09 +0700

    Add login page and home page with post request method

--- proj/app/urls.py
+++ proj/app/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('send_message/', views.send_messages, name='send messages')
 ]
--- proj/proj/urls.py
+++ proj/proj/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView # new
 
 urlpatterns = [
     path('app/', include('app.urls')),
     path('admin/', admin.site.urls),
     path('account/', include('django.contrib.auth.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home')
 ]
